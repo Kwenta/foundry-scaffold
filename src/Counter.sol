dfe56b16c3468e70cb5c475322855cc22285cc9e
tommyrharper <thomasrobertharper@gmail.com>
2024-05-25 16:50:40 +0100

    ⚙️ Update to solidity 0.8.25

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.25;
 
 /// @title Kwenta Example Contract
 /// @notice Responsible for counting
--- test/utils/Bootstrap.sol
+++ test/utils/Bootstrap.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.18;
+pragma solidity 0.8.25;
 
 import {console2} from "lib/forge-std/src/console2.sol";
 import {
